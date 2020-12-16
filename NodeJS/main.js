81da45bd8d4d4fc3c6deb7cb569e99ad89433302
Mindaugas Andrulevicius <mindaugas.andrulevicius@gmail.com>
2020-12-16 20:57:19 +0200

    light cleanup, refactor, add list functionality

--- NodeJS/main.js
+++ NodeJS/main.js
@@ -11,11 +11,12 @@ const sqlString = require('sqlstring'); //mysql also has .escape
 const WEBSITE_PORT = process.env.PORT || 3000;
 //const STATUS_OK_CODE = 200 //feels like overkill to use this
 
-//const CREATION_INPUT_FIELD_NAME = 'gameridnew'
 const USER_FIELD = 'gamerid';
-//const CREATION_INPUT_PASSWORD_FIELD_NAME = 'gamerpasswordnew'
-// This just might be too much... Consider a different naming approach
 const PASSWORD_FIELD = 'gamerpassword';
+const INDEX_SITE = 'index.html';
+const LOGGED_IN_SITE = 'logged_in.html';
+const USER_LIST_SITE = 'user_list.html';
+const WELCOME_MESSAGE = 'Welcome. Create account or login.';
 
 const mysqlHost = process.env.MYSQL_HOST || 'localhost'; //how does this || work?
 const mysqlPort = process.env.MYSQL_PORT || '3306';
@@ -31,8 +32,8 @@ const connectionOptions = {
     user: mysqlUser,
     password: mysqlPassword,
     database: mysqlDatabase
-    //multipleStatements: true
-    //connectionLimit: 10
+    //multipleStatements: true  //Do I need this?
+    //connectionLimit: 10   //This is the default already
 };
 
 console.log('MySQL connection: ', connectionOptions);
@@ -69,34 +70,6 @@ sqlConnection.query('SELECT * FROM web_users', function (error, results, fields)
 // should encrypt password before sending
 
 const server = http.createServer(function (request, response) {
-    /*
-    let body = '';
-    request.on('data', function (chunk){
-        body += chunk;
-    });
-    request.on('end', function(){
-        if ( body !== '') {
-            //console.log(typeof userInput)
-            console.log('POSTed: ' + body);
-            parseUserInput(body);
-            response.writeHead(200); //Why do I need this?
-            // send a response?
-            //response.end();
-    
-            // deal with response in .end(callback)? THIS is .end already
-        };
-    });
-    response.writeHead(200, { 'Content-Type' : 'text/html'});
-    fs.readFile('main.html', function(error, data){
-        if (error) {
-            response.writeHead(404);
-            response.write('Error: File not found');
-        } else {
-            response.write(data);
-        }
-        response.end();
-    });
-    */
     let body = [];
     request.on('error', (error) => {
         console.error(error);
@@ -108,17 +81,13 @@ const server = http.createServer(function (request, response) {
         body = Buffer.concat(body).toString();  // not sure how this works exactly
 
         if (request.method === 'GET' && request.url === '/') {
-            loadPage(response, 'index.html');
+            loadPage(response, INDEX_SITE, WELCOME_MESSAGE);
             //sqlConnection.connect();
         };
     
         if (request.method === 'POST') {
-            //try to create user
-            //send response - account created, login
             console.log('!!POST REQUEST BODY: ', body);
             parseUserInput(body, response, request.url);
-            //parse user input
-            //loadPage(response, 'user_created.html');
         };
     });
     
@@ -134,162 +103,6 @@ const server = http.createServer(function (request, response) {
 });
 
 
-
-
-function loadPage(response, pageName) {
-    response.writeHead(200, { 'Content-Type' : 'text/html'});
-    fs.readFile(pageName, function(error, data){
-        if (error) {
-            response.writeHead(404);
-            response.write('Error: File not found');
-        } else {
-            response.write(data);
-        }
-        response.end();
-    });
-};
-
-
-function createNewUser(credentials, response) {
-    //sqlConnection.connect();
-    let sqlQuery = mysql.format('SELECT * FROM web_users WHERE web_user_name=?',
-        credentials[USER_FIELD]);
-    sqlConnection.query(sqlQuery, function (error, results, fields) {
-        if (error) throw error;
-        let responseString = '';
-    
-        results.forEach(function(data) {
-            responseString += data.ITEM_NAME + ' : ';
-            console.log(data);
-        });
-    
-        console.log('!!CREATE NEW ', responseString);
-
-        if (responseString.length == 0) {
-            insertNewUser(credentials, response);
-        } else {
-            loadPage(response, 'user_exists.html');
-            //sqlConnection.end();
-        };
-    });
-};
-
-
-function insertNewUser(credentials, response) {
-    // I might be triple escaping with format function here, ? placeholders and sqlstring.escape
-    let sqlQuery = mysql.format(
-        'INSERT INTO web_users (web_user_name, web_user_password) VALUES(?, ?)',
-        [credentials[USER_FIELD], credentials[PASSWORD_FIELD]]);
-    sqlConnection.query(sqlQuery, function (error, results, fields) {
-        if (error) {
-            loadPage(response, 'user_creation_failed.html');
-            throw error; // dont want to just throw, should just print error and move on
-        }
-        loadPage(response, 'user_created.html');
-        //sqlConnection.end();
-        //sqlPool.release();??
-
-        /*
-        let responseString = '';
-        //bad, rewrite for insert
-        // is there data in my db now? yes
-        results.forEach(function(data) {
-            responseString += data.ITEM_NAME + ' : ';
-            console.log(data);
-        });
-
-        //results.affectedRows for multiple line inserts
-
-        // should i just catch error, and otherwise go to user_created?
-
-        console.log(responseString);
-        */
-
-
-        /*
-        if (responseString.length == 0) { // check if created
-            loadPage(response, 'user_created.html');
-            
-        } else {
-            loadPage(response, 'user_creation_failed.html');
-        };
-        */
-    });
-};
-  
-
-function loginUser(credentials, response) {
-    let sqlQuery = mysql.format(
-        'SELECT * FROM web_users WHERE web_user_name=?', credentials[USER_FIELD]);
-    sqlConnection.query(sqlQuery, function (error, results, fields) {
-        if (error) throw error;
-        let responseString = '';
-        let responseDict
-
-        //console.log('!!SQL DATA');
-        results.forEach(function(data) {
-            responseString += data.ITEM_NAME + ' : ';
-            //console.log(data);
-            //console.log(data.ITEM_NAME);
-            responseDict = data
-        });
-    
-        //console.log('!!LOGIN ', responseString);
-        //console.log('!!DATA ', responseDict);
-        //console.log('!!CREDENTIALS PASS ', credentials[PASSWORD_FIELD]);
-        //console.log('!!DATABASE PASS ', responseDict['web_user_password']);
-
-        // how to get password from responseString?
-        // why is responseString undefined?
-        // because callback function is still doing its thing when console.log is called?
-        // or because data.ITEM_NAME is not the right call?
-        // - because data.ITEM_NAME is wrong.
-        // but still, why is callback not an issue here?
-
-        if (responseString.length == 0) {
-            loadPage(response, 'user_not_exists.html');
-        } else {
-            if (passwordMatch(credentials[PASSWORD_FIELD], 
-                responseDict['web_user_password'])) {
-                loadPage(response, 'login_successful.html');
-            } else {
-                loadPage(response, 'wrong_password.html');
-            };
-        };
-    });    
-};
-
-
-function passwordMatch(inputPassword, dbPassword) {
-    if (inputPassword === dbPassword) {
-        return true;
-    };
-    return false;
-};
-
-
-function listUsers(response) {
-    let sqlQuery = mysql.format('SELECT * FROM web_users');
-    sqlConnection.query(sqlQuery, function (error, results, fields) {
-        if (error) throw error;
-        let responseString = '';
-    
-        results.forEach(function(data) {
-            responseString += data.ITEM_NAME + ' : ';
-            console.log(data);
-        });
-    
-        console.log('!!LIST ', responseString);
-
-        if (responseString.length == 0) {
-            // load message that table is empty
-        } else {
-            // load user list here
-        };
-    }); 
-}
-
-
 // might need to use promises to make it better structured
 function parseUserInput(inputString, response, userAction) {
     let credentials = storeUserInput(inputString);
@@ -299,45 +112,16 @@ function parseUserInput(inputString, response, userAction) {
     } else if (userAction === '/login') {
         loginUser(credentials, response);
     } else if (userAction === '/list') {
-        listUsers(response); //how to pass response into html file?
+        listUsers(response, false);
+    } else if (userAction === '/list_logged_in') {
+        listUsers(response, true);
+    } else if (userAction === '/back_to_index') {
+        loadPage(response, INDEX_SITE, WELCOME_MESSAGE);
+    } else if (userAction === '/back_to_logged_in') {
+        loadPage(response, LOGGED_IN_SITE, 'welcome back from the list');
+    } else if (userAction === '/logout') {
+        loadPage(response, INDEX_SITE, WELCOME_MESSAGE);
     };
-    /*
-    let new_user;
-    for (let key in credentials) {
-        if (key.search(LOGIN_INPUT_FIELD_NAME) !== -1) {
-            new_user = is_new_user(key);
-            break;
-        };
-    };
-    if (new_user === null) {
-        console.log('unindetified user input ', inputString);
-    } else if (new_user) {
-
-    if (newUser) {
-        if (user_exists(credentials[CREATION_INPUT_FIELD_NAME])) {
-            console.log('user already exists: ', 
-                credentials[CREATION_INPUT_FIELD_NAME]);
-        } else {
-            //validate_password
-            create_new_user(credentials[CREATION_INPUT_FIELD_NAME], 
-                credentials[CREATION_INPUT_PASSWORD_FIELD_NAME]);
-        };
-    } else {
-        if (user_exists(credentials[LOGIN_INPUT_FIELD_NAME])) {
-                let can_login = is_password_ok(credentials[CREATION_INPUT_FIELD_NAME], 
-                    credentials[CREATION_INPUT_PASSWORD_FIELD_NAME])
-                if (can_login) {
-                    //proceed to logged in page
-                    //but NOT IN PARSE FUNCTION
-                } else {
-                    console.log('incorrect password');
-                };
-        } else {
-            console.log('user not found',
-                credentials[LOGIN_INPUT_FIELD_NAME]);
-        };
-    };
-    */
 };
 
 
@@ -366,63 +150,174 @@ function sanitizeUserInput(userInput) {
     return sqlString.escape(userInput);
 };
 
-/*
-// solved by checking action url
-function is_new_user(inputName) { 
-    // change this to receive action value
-    // <input type="hidden" name="action" value="login" />
-    if (inputName === CREATION_INPUT_FIELD_NAME) {
-        return true;
-    } else if (inputName === LOGIN_INPUT_FIELD_NAME) {
-        return false;
-    };
-    return null
-};
-*/
 
-/*
-OLD CODE
-// might be too many seperate calls to db, should use less?
-// then would have unnecessary data
-function user_exists(userName) {
-    //check if user exists in db
-    sqlQuery = mysql.format('SELECT * FROM web_users WHERE web_user_name=?', userName)
+function loadPage(response, pageName) {
+    response.writeHead(200, { 'Content-Type' : 'text/html'});
+    fs.readFile(pageName, function(error, data){
+        if (error) {
+            response.writeHead(404);
+            response.write('Error: File not found');
+        } else {
+            response.write(data);
+        }
+        response.end();
+    });
+};
+
+
+//same name function is ok as long as it has different parameters?
+function loadPage(response, pageName, message) {
+    response.writeHead(200, { 'Content-Type' : 'text/html'});
+    fs.readFile(pageName, function(error, data){
+        if (error) {
+            response.writeHead(404);
+            response.write('Error: File not found');
+        } else {
+            let htmlString = insertMessageIntoHtml(data.toString(), message);
+            response.write(htmlString);
+        }
+        response.end();
+    });
+};
+
+
+function loadPage(response, pageName, message, previousPage) {
+    response.writeHead(200, { 'Content-Type' : 'text/html'});
+    fs.readFile(pageName, function(error, data){
+        if (error) {
+            response.writeHead(404);
+            response.write('Error: File not found');
+        } else {
+            console.log('MESSAGE ', message);
+            console.log('RPEVIOUS PAGE ', previousPage);
+            let htmlString = insertMessageIntoHtml(data.toString(), message);
+            if (pageName === USER_LIST_SITE) {
+                // if unnecessary, but this is still a mess so might as well leave it
+                htmlString = htmlString.replace('${page}', previousPage)
+            };
+            console.log(htmlString);
+            response.write(htmlString);
+        }
+        response.end();
+    });
+};
+
+
+function insertMessageIntoHtml(htmlString, message){
+    // user regex?
+    return htmlString.replace('${message}', message)
+};
+
+
+function createNewUser(credentials, response) {
+    //sqlConnection.connect();
+    let sqlQuery = mysql.format('SELECT * FROM web_users WHERE web_user_name=?',
+        credentials[USER_FIELD]);
     sqlConnection.query(sqlQuery, function (error, results, fields) {
         if (error) throw error;
         let responseString = '';
     
         results.forEach(function(data) {
             responseString += data.ITEM_NAME + ' : ';
+            //this prob results in undefined responseString
             console.log(data);
         });
     
-        //if (responseString.length == 0) {
-        //    responseString = 'No records found';
-        //};
-    
-        console.log(responseString);
+        console.log('!!CREATE NEW ', responseString);
+
+        if (responseString.length == 0) {
+            insertNewUser(credentials, response);
+        } else {
+            loadPage(response, INDEX_SITE, 'User name taken. Choose a different one.');
+            //sqlConnection.end();
+        };
     });
-    // how to get data out of callback function?
-
-    // im really structuring the code wrong
-
-    // could use promises
-
-    // first lets make it simple please
 };
 
 
-function create_new_user(userName, userPassword) {
-    //creates new user
+function insertNewUser(credentials, response) {
+    // I might be triple escaping with format function here, ? placeholders and sqlstring.escape
+    let sqlQuery = mysql.format(
+        'INSERT INTO web_users (web_user_name, web_user_password) VALUES(?, ?)',
+        [credentials[USER_FIELD], credentials[PASSWORD_FIELD]]);
+    sqlConnection.query(sqlQuery, function (error, results, fields) {
+        if (error) {
+            loadPage(response, INDEX_SITE, 'User creation failed.');
+            throw error; // dont want to just throw, should just print error and move on
+        }
+        loadPage(response, INDEX_SITE, 'Account created succesfully. Can login now.');
+    });
+};
+  
+
+function loginUser(credentials, response) {
+    let sqlQuery = mysql.format(
+        'SELECT * FROM web_users WHERE web_user_name=?', credentials[USER_FIELD]);
+    sqlConnection.query(sqlQuery, function (error, results, fields) {
+        if (error) throw error;
+        let responseString = '';
+        let responseDict
+
+        results.forEach(function(data) {
+            responseString += data.ITEM_NAME + ' : ';
+            responseDict = data
+        });
+    
+        // how to get password from responseString?
+        // why is responseString undefined?
+        // because callback function is still doing its thing when console.log is called?
+        // or because data.ITEM_NAME is not the right call?
+        // - because data.ITEM_NAME is wrong.
+        // but still, why is callback not an issue here?
+
+        // this relies on responseString being empty instead of containing undefined value..
+        if (responseString.length == 0) {
+            loadPage(response, INDEX_SITE, `Login failed. User ${credentials[USER_FIELD]} does not exist.`);
+        } else {
+            if (passwordMatch(credentials[PASSWORD_FIELD], 
+                responseDict['web_user_password'])) {
+                loadPage(response, LOGGED_IN_SITE, 'Login successful.');
+            } else {
+                loadPage(response, INDEX_SITE, 'Login failed. Wrong password.');
+            };
+        };
+    });    
 };
 
 
-function is_password_ok(userName, userPassword) {
-    //check if password is ok
+function passwordMatch(inputPassword, dbPassword) {
+    if (inputPassword === dbPassword) {
+        return true;
+    };
+    return false;
 };
 
 
-function validate_password(password) {
-    //check if password meets minimum requirements
+function listUsers(response, loggedIn) {
+    let sqlQuery = mysql.format('SELECT * FROM web_users');
+    sqlConnection.query(sqlQuery, function (error, results, fields) {
+        if (error) throw error;
+        let responseString = '';
+    
+        results.forEach(function(data) {
+            responseString += data['web_user_name'] + '\n';
+            console.log(data);
+        });
+    
+        console.log('!!LIST ', responseString);
+
+        if (responseString.length == 0) {
+            loadPage(response, INDEX_SITE, 'No users found.');
+            // cannot be empty if user logged in
+        } else {
+            // load user list here
+            // if logged in, load with backtologgin
+            // else load with backtoindex
+            if (loggedIn) {
+                loadPage(response, USER_LIST_SITE, responseString, 'logged_in')
+            } else {
+                loadPage(response, USER_LIST_SITE, responseString, 'index')
+            }
+        };
+    }); 
 }
-*/
